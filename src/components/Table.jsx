522a4a0acf6ebbf53bac4599fe2304ba68f55f67
alena <safronovaa1807@gmail.com>
2022-12-02 10:14:55 +0300

    merge

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
-import multirouter from '../routes/multrouter';
+//import multirouter from '../routes/multrouter';
 
 export default function Table({ user }) {
   const [img, setImg] = UseState(null);
@@ -19,47 +19,47 @@ export default function Table({ user }) {
   //   } catch (error) { console.log('ищи ошибку ТУТ!!!!!'); }
   // });
 
+  //   return (
+  //     <div className="App">
+  //       <div className="avatar">
+  //         {
+  // avatar
+  //   ? <img className="logo" src={`${avatar}`} alt="avatar" />
+  //   : <img className="logo" src={`${logo}`} alt="avatar" />
+  // }
+  //       </div>
+  //       <input type="file" onChange={(e) => setImg(e.target.files[0])} />
+  //       <button className="btn" onClick={sendFile}>изменить аватар</button>
+  //     </div>
+  //   );
+  // }
+  // спросить у Алены
+  const [count, setCount] = useState([]);
+  useEffect(() => {
+    fetch(`/kab/prog/:${user.id}`)
+      .then((res) => setCount(res));
+  }, []);
   return (
-    <div className="App">
-      <div className="avatar">
-        {
-avatar
-  ? <img className="logo" src={`${avatar}`} alt="avatar" />
-  : <img className="logo" src={`${logo}`} alt="avatar" />
-}
-      </div>
-      <input type="file" onChange={(e) => setImg(e.target.files[0])} />
-      <button className="btn" onClick={sendFile}>изменить аватар</button>
+    <div className="container">
+      {/* <table className="score">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">Count</th>
+        </tr>
+      </thead>
+      <th scope="row">{ id }</th>
+      <td>{ id }</td>
+      <td>{ count }</td>
+    </table> */}
+      {
+      count?.map((el) => (
+        <>
+          <div>{el.theme}</div>
+          <div>{el.count}</div>
+        </>
+      ))
+    }
     </div>
   );
 }
-// спросить у Алены
-// const [count, setCount] = useState([]);
-// useEffect(() => {
-//   fetch(`/kab/prog/:${user.id}`)
-//     .then((res) => setCount(res));
-// }, []);
-// return (
-//   <div className="container">
-//     {/* <table className="score">
-//       <thead>
-//         <tr>
-//           <th scope="col">id</th>
-//           <th scope="col">Count</th>
-//         </tr>
-//       </thead>
-//       <th scope="row">{ id }</th>
-//       <td>{ id }</td>
-//       <td>{ count }</td>
-//     </table> */}
-//     {
-//       count?.map((el) => (
-//         <>
-//           <div>{el.theme}</div>
-//           <div>{el.count}</div>
-//         </>
-//       ))
-//     }
-// </div>
-// );
-// }
